ba017cb6f8149669e5a5a0f4237a410ffb7eed87
Etienne Martin <apple@emartin.ca>
2026-02-26 15:30:14 +0000

    fix: octokit token check

--- src/github/octokit.ts
+++ src/github/octokit.ts
@@ -2,15 +2,10 @@ import { getOctokit as octokit } from '@actions/github';
 import { inputs } from './input';
 
 export const getOctokit = (...args: [string] | [undefined] | []): ReturnType<typeof octokit> => {
-  if (args.length === 0) {
-    return getOctokit(inputs.githubToken);
-  }
-
   const [token] = args;
 
-  if (token === undefined) {
-    throw new Error('Missing GitHub token.');
-  }
+  if (!args.length) return getOctokit(inputs.githubToken);
+  if (!token) throw new Error('Missing GitHub token.');
 
   return octokit(token);
 };
